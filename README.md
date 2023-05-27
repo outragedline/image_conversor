71ebe6d75105a5093cd6f96f262906903a3769a1
outragedline <outragedline@proton.me>
2023-05-27 14:20:41 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Its a simple python script i made to convert 300 images
+
+just run `python conversor path` to convert all files in this path
+
+it will clone your directory tree structure in a directory named `converted`
---
+++ conversor.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import subprocess
+
+path = sys.argv[1]
+
+if not os.path.isdir(path):
+    print("Directory doesn't exist")
+    sys.exit(1)
+
+def convert(source_directory):
+    output_dir = os.path.join(os.getcwd(), "converted")
+
+    for root, dirs, files in os.walk(source_directory):
+        relative_path = os.path.relpath(root, source_directory)
+        target_dir = os.path.join(output_dir, relative_path)
+        os.makedirs(target_dir, exist_ok=True)
+
+    for root, dirs, files in os.walk(source_directory):
+        relative_path = os.path.relpath(root, source_directory)
+        target_dir = os.path.join(output_dir, relative_path)
+        for file in files:
+            output_file_name = os.path.splitext(file)[0]
+            output_file_path = os.path.join(target_dir, output_file_name + ".jpg")
+            input_file_path = os.path.join(root, file)
+            subprocess.run(['ffmpeg', '-i', input_file_path, output_file_path])
+
+convert(path)
